47ab25fd6d69f35cc44e3b59371eddf0b8ee8d06
DESKTOP-GV16N45\Alumno <informatica@esne.es>
2025-05-21 21:03:43 +0200

    mlflow included

--- scripts/train_val_model.py
+++ scripts/train_val_model.py
@@ -1,25 +1,22 @@
-# from ultralytics import YOLO
-
-# # Load the YOLOv8n model (pre-trained or from scratch)
-# model = YOLO(r"C:\Users\Alumno.DESKTOP-GV16N45.000\AppData\UDIT\cfg\model\yolov8.yaml")  # Path to your pre-trained or custom YOLOv8n model
-
-# model.train(data = r"C:\Users\Alumno.DESKTOP-GV16N45.000\AppData\output\dataset.yaml", epochs = 100, imgsz = 640)
-
-from ultralytics import YOLO
+from ultralytics import YOLO, settings
+import mlflow
 
 def main():
-    # Load the YOLOv8n model
-    model = YOLO(r"C:\Users\Alumno.DESKTOP-GV16N45.000\AppData\UDIT\cfg\model\yolov8.yaml")
+    # Set valid MLflow tracking URI
+    mlflow.set_tracking_uri("file:///C:/Users/Alumno.DESKTOP-GV16N45.000/AppData/UDIT/runs/mlflow")
 
-    # Train the model
+    # Enable logging
+    settings.update({"tensorboard": True, "mlflow": True})
+
+    model = YOLO("yolov8n.pt")
+
     model.train(
         data=r"C:\Users\Alumno.DESKTOP-GV16N45.000\AppData\output\dataset.yaml",
+        cfg=r"C:\Users\Alumno.DESKTOP-GV16N45.000\AppData\UDIT\cfg\default.yaml",
         epochs=100,
         imgsz=640,
         name="yolov8n_custom_train"
     )
 
 if __name__ == "__main__":
-    # import multiprocessing
-    # multiprocessing.freeze_support()  # Safe for frozen executables
     main()
